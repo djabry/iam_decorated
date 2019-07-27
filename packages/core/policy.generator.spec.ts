4e4194d5cec2f16df3916bf237f45f2ee2a9f528
Daniel Jabry <daniel.jabry@rezonence.com>
2019-07-27 09:19:16 +0100

    Add a stub test for the policy generator

---
+++ packages/core/policy.generator.spec.ts
@@ -0,0 +1,30 @@
+import {expect} from "chai";
+import {Effect} from "./effect";
+import {PolicyGenerator} from "./policy.generator";
+import {Statement} from "./statement";
+
+describe("Policy generator", () => {
+    let instance: PolicyGenerator;
+
+    beforeEach(() => {
+        instance = new PolicyGenerator();
+    });
+
+    it("Constructs a policy object that includes all the input statements", () => {
+        const statements: Statement[] = [
+            {
+                Action: "foo",
+                Effect: Effect.Allow,
+                Resource: "bar",
+            },
+            {
+                Action: "bar",
+                Effect: Effect.Allow,
+                Resource: "baz",
+            },
+        ];
+        const policies = instance.toPolicies(statements);
+        const firstPolicy = policies.shift();
+        expect(firstPolicy.Statement).to.deep.equal(statements);
+    });
+});
